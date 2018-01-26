62be5caed33be0228473467614e775456a6c04d6
Federico Barresi <federico.barresi@evopro-ag.de>
2018-01-26 21:56:36 +0100

    adapted pack.ps1

--- _nuspec/pack.ps1
+++ _nuspec/pack.ps1
@@ -1,4 +1,4 @@
-$version = [System.Reflection.Assembly]::LoadFile("..\out\Release\Sharp7.dll").GetName().Version
+$version = [System.Reflection.Assembly]::LoadFile("out\Release\Sharp7.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
@@ -6,6 +6,6 @@ Write-Host "Setting .nuspec version tag to $versionStr"
 $content = (Get-Content Sharp7.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File Sharp7.compiled.nuspec
+$content | Out-File _nuspec\Sharp7.compiled.nuspec
 
-& NuGet.exe pack Sharp7.compiled.nuspec
+& _nuspec\NuGet.exe pack _nuspec\Sharp7.compiled.nuspec
